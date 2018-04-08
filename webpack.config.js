027794f3766b7f39f98c66663c5967c7a1f72e8e
Kelley Jenkins <ckjenkins3@gmail.com>
2018-04-08 15:09:16 -0600

    adds webpack for githubpages

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: {
+    main: "./main.js",
+  },
+  output: {
+    filename: "main.bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
+    ]
+  }
+}

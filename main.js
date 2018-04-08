23d5e6fbfae0a17ce8bc37c01c896dd98a2e5254
Kelley Jenkins <ckjenkins3@gmail.com>
2018-04-08 11:29:32 -0600

    appends unique data id for form

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-
-
+let clicks = 6
 $('.add-ingredient').on('click', function(){
-   $('.ingredient-form-inputs').append(`<input title='ingredient-name' label='ingredient-name'type='text' class='name-input' placeholder='Ingredient'></br>`)
+   $('.ingredient-form-inputs').append(`<input title='ingredient-name' label='ingredient-name'type='text' data-id="${clicks}" class='name-input' placeholder='Ingredient'></br>`)
+   clicks++
 })
+

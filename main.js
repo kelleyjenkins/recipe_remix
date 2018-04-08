8f0028377713375f244f53040f9aafb3f9246467
Kelley Jenkins <ckjenkins3@gmail.com>
2018-04-08 10:55:03 -0600

    adds form to home page, and ability to add an extra ingredient input on click

---
+++ main.js
@@ -0,0 +1,5 @@
+
+
+$('.add-ingredient').on('click', function(){
+   $('.ingredient-form-inputs').append(`<input title='ingredient-name' label='ingredient-name'type='text' class='name-input' placeholder='Ingredient'></br>`)
+})
